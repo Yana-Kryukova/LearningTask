70380fabffacf540375eadee4aa8a7b63c1fc030
Yana-Kryukova <krjanik@icloud.com>
2024-11-25 22:48:54 +0700

    Задача решена.

---
+++ ReadFilesAsync/ReadFilesAsync/Program.cs
@@ -0,0 +1,63 @@
+﻿using ReadFilesAsync.Base;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ReadFilesAsync
+{
+    internal class Program(IReaderService<int> service)
+    {
+        public async Task GetCountCharacterInFiles1(string directory, CancellationToken cancellationToken)
+        {
+            // Only get files that are text files only as you want only .txt 
+            string[] dirs = Directory.GetFiles(directory, "*.txt");
+            foreach (var file in dirs)
+            {
+                var res = await service.ReadAsync(file, cancellationToken);
+                Console.WriteLine($"In file {file} is {res} spaces");
+            }
+        }
+        public async Task GetCountCharacterInFiles2(string directory, CancellationToken cancellationToken)
+        {
+            // Only get files that are text files only as you want only .txt 
+            string[] dirs = Directory.GetFiles(directory, "*.txt");
+            var list = new List<Task<int>>();
+            foreach (var file in dirs)
+            {
+                list.Add(service.ReadAsync(file, cancellationToken));
+            }
+            Task.WaitAll(list.ToArray());
+            int i = 0;
+            foreach (var res in list)
+            {
+                Console.WriteLine($"In file {i+1} is {res.Result} spaces");
+            }
+
+        }
+        static async Task Main(string[] args)
+        {
+            var p = new Program(new ReaderAllTextService());
+            var directory = Path.GetFullPath("../../../TestDirectory");
+
+            var watch = new Stopwatch();
+            watch.Start();
+            await p.GetCountCharacterInFiles1(directory, CancellationToken.None);
+            watch.Stop();
+            WriteWatch(watch);
+            watch.Start();
+            await p.GetCountCharacterInFiles2(directory, CancellationToken.None);
+            watch.Stop();
+            WriteWatch(watch);
+        }
+        private static void WriteWatch(Stopwatch w)
+        {
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = w.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
---
+++ ReadFilesAsync/ReadFilesAsync/ReaderAllTextService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using ReadFilesAsync.Base;
+
+namespace ReadFilesAsync
+{
+    public class ReaderAllTextService : IReaderService<int>
+    {
+        public async Task<int> ReadAsync(string path, CancellationToken cancellationToken)
+        {
+            if (!File.Exists(path))
+                return -1;
+
+            var res = await File.ReadAllTextAsync(path, cancellationToken);
+            return CalculateCharacterInString(res);
+        }
+
+        private int CalculateCharacterInString(string str, char character = ' ')
+        => str.Count(x => x == character);
+    }
+}
